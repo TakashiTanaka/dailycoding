abd984b3e43760e36a81bbacdd0bb9bc69011527
TakashiTanaka <TakashiTanaka@users.noreply.github.com>
2022-09-08 13:27:59 +0000

    deploy: 76acb20680fa28cda8f228758fab14eeb8e8ef31

---
+++ sketches/2022/62/src/global.d.ts
@@ -0,0 +1,17 @@
+import type p5 from 'p5';
+
+declare global {
+  var createCanvas: typeof p5.prototype.createCanvas;
+  var colorMode: typeof p5.prototype.colorMode;
+  var textFont: typeof p5.prototype.textFont;
+  var background: typeof p5.prototype.background;
+  var createVector: typeof p5.prototype.createVector;
+  var random: typeof p5.prototype.random;
+  var setup: typeof p5.prototype.setup;
+  var noise: typeof p5.prototype.noise;
+  var color: typeof p5.prototype.color;
+  var width: typeof p5.prototype.width;
+  var height: typeof p5.prototype.height;
+  var HSB: typeof p5.prototype.HSB;
+  var TAU: typeof p5.prototype.TAU;
+}
